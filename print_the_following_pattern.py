f8592acb3a8d66d9b87d99b4a6e63399f5057c22
srikanth7666 <109718354+srikanth7666@users.noreply.github.com>
2022-11-05 11:23:25 +0530

    n=int(input()) for i in range(n):     for j in range(n):         print(chr(65+i),end=' ')     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,8 +1,5 @@
 n=int(input())
-for i in range(1,n+1):
-    for j in range(1,n+1):
-        if(i==j or j==n or j==1):
-            print('*',end=' ')
-        else:
-            print(' ',end=' ')
+for i in range(n):
+    for j in range(n):
+        print(chr(65+i),end=' ')
     print()
