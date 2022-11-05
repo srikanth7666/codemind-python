396de3a962909959a5b391f44f25b60b33daed18
srikanth7666 <109718354+srikanth7666@users.noreply.github.com>
2022-11-05 16:35:19 +0530

    def digsum(n):     sum=0     while(n>0 or sum>9):         if(n==0):             n=sum             sum=0         sum+=n%10         n//=10     return sum n=int(input()) print(digsum(n))

--- Add_Digits.py
+++ Add_Digits.py
@@ -1,12 +1,12 @@
-def da(n):
-  while(n>9):
-   s=0
-   while(n>0):
-    r=n%10
-    s+=r
-    n=n//10
-   n=s
-  return s
-n=int(input())  
-m=da(n)
-print(m)
+def digsum(n):
+    sum=0
+    while(n>0 or sum>9):
+        if(n==0):
+            n=sum
+            sum=0
+        sum+=n%10
+        n//=10
+    return sum
+n=int(input())
+print(digsum(n))
+    
